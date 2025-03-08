2602296492e3543e5e8075e34d66a7c1760e4da6
tawsifahmed <tawsifahmed@iut-dhaka.edu>
2025-03-09 02:42:13 +0600

    reservation api updated

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\RoomController;
 use App\Http\Controllers\API\FloorController;
-
+use App\Http\Controllers\API\ReservationController;
 
 /*
 |--------------------------------------------------------------------------
